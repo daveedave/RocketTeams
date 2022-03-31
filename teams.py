a869c1f64bec539c2929a1d8fd02db9f60397680
daveedave <davascha@web.de>
2022-03-31 19:14:55 +0200

    v.1.1

--- teams.py
+++ teams.py
@@ -1,15 +1,27 @@
 import random
 
-players = ("David", "Raphi", "Lukas","Adrian", "Felix")
+players = ("Kadda", "Raphi", "Lukas","Adrian", "Felix")
 temp = []
 teamsize = 2
 
 
 for i in range(100):
-    
     tup = tuple(random.sample(players, teamsize))
     temp.append(tup)
-       
-teams = list({*map(tuple, map(sorted, temp))})
 
-print(teams)
+     
+team_1 = list({*map(tuple, map(sorted, temp))})
+team_2 = []
+
+
+for team in team_1:
+    temp_2 = list(players)
+    for name in team:
+        temp_2.remove(name)     
+    team_2.append(temp_2)
+
+
+
+for i in range(len(team_1)):
+    print("Runde : " + str(i + 1))
+    print("Team 1 ist : " + str(team_1[i]) + " und Team 2 ist: " + str(team_2[i]))
